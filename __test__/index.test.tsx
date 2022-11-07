9aa6adb3e220d609828ed50480da3c7a3a896c8b
toyesebalogun <33703095+toyesebalogun@users.noreply.github.com>
2022-11-07 23:25:18 +0000

    Delete __test__directory

--- __test__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import CheckboxColumn from ".."
-import { render } from "@testing-library/react";
-
-test("checkbox column", () => {
-    it("should match snapshot", () =>{
-        const component = render(<CheckboxColumn/>);
-        expect(component).toMatchSnapshot();
-    })
-})
